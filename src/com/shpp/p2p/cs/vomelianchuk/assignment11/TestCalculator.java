9617897a49405dcc6fd54b52e17b4b2a56ffa077
Volodymyr Omelianchuk <omelianchuk921@gmail.com>
2023-11-07 17:57:25 +0200

    final

--- src/com/shpp/p2p/cs/vomelianchuk/assignment11/TestCalculator.java
+++ src/com/shpp/p2p/cs/vomelianchuk/assignment11/TestCalculator.java
@@ -7,13 +7,14 @@ package com.shpp.p2p.cs.vomelianchuk.assignment11;
  */
 public class TestCalculator {
     public static void main(String[] args) {
+        
         Assignment11Part1.main(new String[]{"3 - a*(5 /(-4 - 1)) / b / (3-4)", "a = -3", "b = 3"});
         Assignment11Part1.main(new String[]{"3 - (a*5) /4 - 1 * b + 3-4", "a = 2", "b = 3"});
         Assignment11Part1.main(new String[]{"3 - (a*(4*(4+1)))", "a = 2"});
-        Assignment11Part1.main(new String[]{"-((-3)^-2 - (a*(4*(4^b)))^2)", "a = 2", "b = 3"});
         Assignment11Part1.main(new String[]{"1 + cos(a) - sin(-1)", "a = 2", "b = 3"});
         Assignment11Part1.main(new String[]{"5-sin(45*cos(a))", "a = 2"});
-        Assignment11Part1.main(new String[]{"1+(2 +3*(4+5-sin(45*cos(a))))/7", "a = 2"});
-        Assignment11Part1.main(new String[]{"log2(log10(a)/sqrt(b/4))", "a = 100000", "b = 16"});
+
+        Assignment11Part1.main(new String[]{"1+(2 +3*(4+5-sin(45*cos(4))))/a", "a = 2"});
+        Assignment11Part1.main(new String[]{"-((-3)^-2 - (a*(4*(4^b)))^2)", "a = 2", "b = 3"});
     }
 }
